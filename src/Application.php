92c2f446ca53d9b1e63c4dc82416bca69ea25def
wtdl <szpengjian@gmail.com>
2019-01-25 18:01:28 +0800

    fix: 基础文件

---
+++ src/Application.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 深圳市易知互联网科技有限公司
+ * User: pengjian(szpengjian@gmail.com)
+ * Date: 19-1-25
+ * Time: 下午5:41
+ */
+namespace Wtdl\Framework;
+
+class Application{
+
+
+    public function __construct()
+    {
+        echo "Application";
+    }
+
+}
